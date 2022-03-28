373722938d9081bd6030329b2622cb65565b6c33
Hugo AVRIL <hugo.avril@outlook.fr>
2022-03-28 11:50:28 +0200

    connexion admin ok
    Co-authored-by: Alexis Villy <Alexvillex@users.noreply.github.com>
    Co-authored-by: Guillaume LEFEBVRE <Guiiillaume@users.noreply.github.com>

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,20 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Form\LoginForm;
+use App\Form\Login;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\VarDumper\VarDumper;
 
 class AccueilController extends AbstractController {
 
     /**
-     * @Route("Accueil", name="accueil")
+     * @Route("Accueil", name="accueil", methods={"POST", "GET"})
      */
 
-    function login(ManagerRegistry $doctrine) {
-        return $this->render('accueil.html.twig');
+    // function login(ManagerRegistry $doctrine) {
+    //     return $this->render('accueil.html.twig');
+    // }
+
+    function loginAction(Request $request, ManagerRegistry $doctrine) {
+        $form = $this->createForm(Login::class);
+        $form->handleRequest($request);
+
+        $username = isset($_POST['login'])?$_POST['login']:'';
+        $password = isset($_POST['password'])?$_POST['password']:'';
+        if ($username == 'admin' && $password == 'admin') {
+            $this->addFlash('success', 'Bienvenue');
+            return $this->redirectToRoute('Entreprise');
+        } else {
+            $this->addFlash('error', "Nom d'Utilisateur ou mot de passe incorrect");
+        }
+
+        return $this->render('accueil.html.twig', [
+                    'form' => $form->createView()
+        ]);
     }
 }
--- src/Form/LoginForm.php
+++ src/Form/LoginForm.php
@@ -9,14 +9,14 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-// NOTE insérer les types de données ici
+
 class Login extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('login', TextType::class)
-            ->add('password', PasswordType::class)
+            ->add('password', TextType::class)
             ->add('submit', SubmitType::class)
             ->getForm();
     }
