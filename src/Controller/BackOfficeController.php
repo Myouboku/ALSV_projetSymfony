f3f2cecdf2c96c92fbf312efb8e13aeffbeb0e1c
Alexvillex <alexisvilly.work@gmail.com>
2022-03-30 08:29:16 +0200

    back office

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -7,14 +7,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Persistence\ManagerRegistry;
 
 
 class BackOfficeController extends AbstractController {
   /**
    * @Route("BackOffice", name="backoffice")
    */
-  function backoffice() {
-    return $this->render('backoffice.html.twig');
+    public function backoffice(ManagerRegistry $doctrine) {
+    $stmt = $doctrine->getConnection()->prepare('SELECT * FROM entreprise');
+    $ListeUser = $doctrine->getConnection()->prepare('SELECT uti_username from utilisateur');
+    $ListeProfil = $doctrine->getConnection()->prepare('SELECT * FROM personne');
+    $result = $stmt->execute();
+    $resultProfil = $ListeProfil->execute();
+    $resultUser = $ListeUser->execute();
+    return $this->render('backoffice.html.twig',[ 'entreprise' => $result->fetchAll() , 'user' => $resultUser->fetchAll(), 'personne' => $resultProfil->fetchAll()] );
   }
 }
 ?>
