24847c0559752b11f28e94403eae66b22bdd8804
Kenzo <94042385+TrafiKen@users.noreply.github.com>
2022-03-31 08:39:26 +0200

    1 commit recherche (non fonctionnel)

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Persistence\ManagerRegistry;
 
 
 class BackOfficeController extends AbstractController {
@@ -15,6 +16,21 @@ class BackOfficeController extends AbstractController {
    */
   function backoffice() {
     return $this->render('backoffice.html.twig');
+  }
+
+  public function recherche_entreprise(ManagerRegistry $doctrine)
+  {
+    $recherche = isset($_POST['text_recherche']) ? $_POST['text_recherche'] : '';
+    $type_recherche = $_POST['search-type'];
+    
+    switch($type_recherche) 
+    {
+      case "raisonSociale":
+        $stmt = $doctrine->getConnection()->prepare('SELECT * FROM entreprise');
+        $result = $stmt->execute();
+        return $this->render('entreprise.html.twig',[ 'entreprise' => $result->fetchAll()] );
+        break;
+    }
   }
 }
 ?>
