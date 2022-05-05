2e32b32f876ad793b81ee2395813e89d851601df
Alexvillex <alexisvilly.work@gmail.com>
2022-05-05 08:43:04 +0200

    Liste du Back office + modifs

--- public/ressources/js/ListShow.js
+++ public/ressources/js/ListShow.js
@@ -1,29 +1,29 @@
 
-function ListShow() {
-  let BtnEntreprise = document.getElementById("btnEntreprise");
-  let BtnUser = document.getElementById("btnUser");
-  let BtnTutor = document.getElementById("btnTuteur");
+const ListShow = (() => {
+  const BtnEntreprise = document.getElementById("btnEntreprise");
+  const BtnUser = document.getElementById("btnUser");
+  const  BtnTutor = document.getElementById("btnTuteur");
   
-  let Entreprise = document.getElementById("entreprise");
-  let User = document.getElementById("utilisateur");
-  let Tuteur = document.getElementById("tuteur");
+  const Entreprise = document.getElementById("entreprise");
+  const User = document.getElementById("utilisateur");
+  const Tuteur = document.getElementById("tuteur");
 
-  BtnEntreprise.addEventListener("click", function(){
+  BtnEntreprise.addEventListener("click", (() => {
     Entreprise.style.display = "block";
     User.style.display = "none";
     Tuteur.style.display = "none";
-  });
+  }));
   
-  BtnUser.addEventListener("click", function(){
+  BtnUser.addEventListener("click", (() => {
     Entreprise.style.display = "none";
     User.style.display = "block";
     Tuteur.style.display = "none";
-  });
+  }));
 
-  BtnTutor.addEventListener("click", function(){
+  BtnTutor.addEventListener("click", (() => {
     Entreprise.style.display = "none";
     User.style.display = "none";
     Tuteur.style.display = "block";
-  });
-}
+  }));
+})
 
