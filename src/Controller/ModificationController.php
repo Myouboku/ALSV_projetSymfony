73c1826715efe99c181ef24a7f7213d1f5cf077d
Guiiillaume <guillaume.lefebvre@campus-la-chataigneraie.org>
2022-05-10 15:04:58 +0200

    Affichage des données de l'entreprise Récupération des différentes options
    Co-authored-by: Alexis Villy <Alexvillex@users.noreply.github.com>
    Co-authored-by: Hugo AVRIL <Myouboku@users.noreply.github.com>

--- src/Controller/ModificationController.php
+++ src/Controller/ModificationController.php
@@ -6,7 +6,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Exception;
+use Doctrine;
 
 class ModificationController extends AbstractController
 {
@@ -17,39 +17,30 @@ class ModificationController extends AbstractController
 
     function modificationAction(Request $request, ManagerRegistry $doctrine)
     {
-        
-        try {
-            //récupération des données de l'entreprise
-            $Id = intval($_GET['SelectedEnt']);                                
-            $stmt = $doctrine->getConnection()->prepare('CALL PS_Recuperation_EntrepriseId(:IdEntreprise)'); //PS_Recuperation_EntrepriseId
-            $stmt->bindValue(':IdEntreprise', $Id); //bindValue pour une valeur
-            $result = $stmt->execute(); //execution de la requete
-            $stmtOpt = $doctrine->getConnection()->prepare('SELECT id, opt_libelle from entreprise_option');
-            $resultOpt = $stmtOpt->execute();
+        if ($_GET['SelectedEnt']) {
+            $Id = intval($_GET['SelectedEnt']);
             
-            return $this->render('modifEntreprise.html.twig',[ 'entreprise' => $result->fetchAll(), 'opt' => $resultOpt->fetchAll()] ); //affichage de la page de modification
+            //récupération des données de l'entreprise                                            
+            $stmt = $doctrine->getConnection()->prepare('CALL PS_Recuperation_EntrepriseId(:IdEntreprise)'); //PS_Recuperation_EntrepriseId                
+            $stmt->bindValue(':IdEntreprise', $Id);
+            
+            // récupération de la liste des options
+            $stmtOpt = $doctrine->getConnection()->prepare('CALL PS_Recuperation_Option(:IdEntr)'); //PS_Recuperation_Option  
+            $stmtOpt->bindValue(':IdEntr', $Id);
 
+            // exécution et fetch de la requête de récupération des données de l'entreprise
+            $result = $stmt->execute();            
+            $result = $result->fetchAll();
+            // close mysql connection
+            $stmt = null;
+
+            // exécution et fetch de la requête de récupération des options 
+            $resultOpt = $stmtOpt->execute(); 
+            $resultOpt = $resultOpt->fetchAll(); 
+            $stmtOpt = null;  
+
+            // affichage
+            return $this->render('modifEntreprise.html.twig', ['entreprise' => $result, 'opt' => $resultOpt]); //affichage de la page de modification
         }
-        catch (Exception $e) {
-            try {
-                //récupération des données de l'utilisateur
-                $Id = $request->request->get('Id');
-                $stmt = $doctrine->getConnection()->prepare('CALL PS_Recuperation_UtilisateurId(:IdUtilisateur)'); //PS_Recuperation_UtilisateurId
-                $stmt->bindValue(':IdUtilisateur', $Id); //bindValue pour une valeur
-                $result = $stmt->execute();  //execution de la requete
-                return $this->render('modifUtilisateur.html.twig',[ 'utilisateur' => $result->fetchAll()] ); //affichage de la page de modification
-
-            }
-            catch (Exception $e) {
-                
-                //récupération des données du tuteur
-                $Id = $request->request->get('Id');
-                $stmt = $doctrine->getConnection()->prepare('CALL PS_Recuperation_TuteurId(:IdTuteur)'); //PS_Recuperation_TuteurId
-                $stmt->bindValue(':IdTuteur', $Id); //bindValue pour une valeur
-                $result = $stmt->execute();   //execution de la requete
-                return $this->render('modifTuteur.html.twig',[ 'tuteur' => $result->fetchAll()] ); //affichage de la page de modification
-
-            }
-        }   
     }
 }
