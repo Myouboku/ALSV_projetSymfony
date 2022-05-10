49a2999fba1575fb256ce21f783524a9a9d100e2
Hugo AVRIL <hugo.avril@outlook.fr>
2022-05-10 09:02:41 +0200

    redirection auto entreprise

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -19,8 +19,21 @@ class EntrepriseController extends AbstractController {
    */
   public function entreprise(ManagerRegistry $doctrine) 
   {
+    // NOTE si la variable connexion est à false, on redirige vers la page d'accueil
+    if (!$this->get('session')->get('connected')) {
+      return $this->redirectToRoute('accueil');
+    }
+
     $stmt = $doctrine->getConnection()->prepare('SELECT ent_rs,ent_adresse,ent_cp,ent_ville,ent_pays, IFNULL(opt_libelle, "Pas Option") as Opt_Libelle from entreprise LEFT join entreprise_option on entreprise.opt_id = entreprise_option.id;');
     $result = $stmt->execute();
+
+    if($_POST) {
+      if($_POST['deconnectButton']){
+        $this->get('session')->set('connected', false);
+        return $this->redirectToRoute('accueil');
+      }
+    }
+
     return $this->render('entreprise.html.twig',[ 'entreprise' => $result->fetchAll()] );
   }
 }
