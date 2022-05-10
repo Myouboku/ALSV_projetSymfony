bf132996871c17fe03e77cd5ef025502e29874c9
Alexvillex <alexisvilly.work@gmail.com>
2022-05-10 14:49:01 +0200

    merge for rebase
    Co-authored-by: Hugo AVRIL <Myouboku@users.noreply.github.com>
    Co-authored-by: Guillaume LEFEBVRE <Guiiillaume@users.noreply.github.com>

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -10,12 +10,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
 
 
-class BackOfficeController extends AbstractController {
+class BackOfficeController extends AbstractController
+{
   /**
    * @Route("BackOffice", name="backoffice")
    */
     public function backoffice(ManagerRegistry $doctrine) {
-    
+      // NOTE si la variable connexion est à false, on redirige vers la page d'accueil
+      if (!$this->get('session')->get('connected')) {
+        return $this->redirectToRoute('accueil');
+      }
+  
       $stmt = $doctrine->getConnection()->prepare('SELECT entreprise.id as ent_id, ent_rs,ent_adresse,ent_cp,ent_ville,ent_pays, IFNULL(opt_libelle, "-") as Opt_Libelle from entreprise LEFT join entreprise_option on entreprise.opt_id = entreprise_option.id;');
       $ListeUser = $doctrine->getConnection()->prepare('SELECT utilisateur.id as uti_id, uti_username,uti_role from utilisateur');
       $ListeProfil = $doctrine->getConnection()->prepare('SELECT personne.id as per_id,per_nom, per_prenom, IFNULL(Concat(0,per_tel), "-") as per_tel, per_mail FROM personne');
@@ -23,6 +28,7 @@ class BackOfficeController extends AbstractController {
       //$resultEntreprise = $ListEntreprise->execute();
       $resultProfil = $ListeProfil->execute();
       $resultUser = $ListeUser->execute();
+
       if(isset($_POST['SelectedEnt'])) { 
         $Id = isset($_POST['SelectedEnt']) ? $_POST['SelectedEnt'] : 0;
         $DeleteEnt = $doctrine->getConnection()->prepare('CALL PS_D_Utilisateur(:Id) ');
